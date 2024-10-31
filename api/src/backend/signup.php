b617c3558d20301edf5dfa72507b7269f142d053
Bryan0g <trevorbryan78@gmail.com>
2024-10-31 12:27:50 -0500

    all

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -1,18 +1,56 @@
 <?php
 
+
+function save_data_supabase($email, $password){
+    //supabase 
+    $SUPABASE_URL = 'https://eprfivfbtjombptxqxbt.supabase.co';
+    $SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVwcmZpdmZidGpvbWJwdHhxeGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzODg3MDgsImV4cCI6MjA0NTk2NDcwOH0.MBwSEKlpkhze15rfMXH80iwCJehOU3VwP2hJDOOI5L4';
+      
+    $url = "$SUPABASE_URL/rest/v1/users";
+
+    $data =[
+        'email' => $email,
+        'passwd' => $password,
+    ];
+
+    $options = [
+        'http' => [
+            'header'  => [
+                "Content-Type: application/json",
+                "Authorization: Bearer $SUPABASE_KEY",
+                "apikey: $SUPABASE_KEY"
+            ],
+            'method'  => 'POST',
+            'content' => json_encode($data),
+        ],
+    ];
+    
+      
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, false, $context);
+    //$response_data = json_decode($response, true);
+
+    if($response === false) {   
+        echo "Error: unable to save to Supabase";
+        exit;
+    } echo "user has been created." . json_encode($response_data);
+
+
+}
+
 require('../../config/db_conection.php');
 
 // Obtener datos del formulario de registro
 $email = $_POST['email'];
-$pass = $_POST['passwd'];
-$enc_pass = md5($pass);
+$password = $_POST['passwd'];
+$enc_pass = md5($password);
 
 // Verificar si la conexión está abierta y conectarse si no lo está
 $query = "SELECT * FROM users WHERE email = '$email'";
 $result = pg_query($conn, $query);
 $row=pg_fetch_assoc($result);
 
-if ($result) {
+if ($row) {
     echo "<script>alert('Email already exists!')</script>";
     header('refresh:0; url=http://127.0.0.1/beta/api/src/register_form.html');
     exit();
@@ -23,12 +61,16 @@ $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
 $result = pg_query($conn, $query);
 
 if ($result) {
+
+    save_data_supabase($email, $enc_pass);  // Save to Supabase
     echo "<script>alert('Registration successful')</script>";
     header('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
     exit();
 } else {
     echo "Failed to register user: ";
-} pg_close($conn);
+} 
+
+pg_close($conn);
 
 // Cerrar la conexión si está abierta
 /*
@@ -38,3 +80,4 @@ echo "<br>Encrypted Password: " . $enc_pass;
 */
 
 ?>
+
